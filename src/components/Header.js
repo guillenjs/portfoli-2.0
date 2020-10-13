7a7f1f149c765eed680aa5a5b52ea214b5eae2c9
guillenjs <jorgeguilln@gmail.com>
2020-10-13 09:49:35 -0400

    updated icons

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,27 +2,40 @@ import React from 'react'
 
 import Footer from './Footer'
 import avatar from '../assets/images/avatar.jpg'
+import profile from '../assets/images/stackicons/profile.jpg'
+
+import javascript from '../assets/images/stackicons/javascript.png'
+import react from '../assets/images/stackicons/iconfinder_react_1296845.png'
+import ruby from '../assets/images/stackicons/iconfinder_ruby_308447.png'
+import rails from '../assets/images/stackicons/iconfinder_ruby-on-rails_3069735.png'
+import postgresql from '../assets/images/stackicons/iconfinder_postgresql_4691328.png'
+import css3 from '../assets/images/stackicons/iconfinder_css-3_4961937.png'
+import html5 from '../assets/images/stackicons/iconfinder_50-html5_104453.png'
+import github from '../assets/images/stackicons/iconfinder_Github_1298743.png'
+console.log(profile)
+
 
 const Header = () => (
+
   <header id="header">
     <div className="inner">
       <a href="#" className="image avatar">
-        <img src="profile.jpg" alt="profile image" className="profileImg" />
+        <img src={profile} alt="profile image" className="profileImg" />
       </a>
       <h1>
         <strong>Full Stack Developer</strong>
       </h1>
       <br></br>
       <u><h4>Stack</h4></u>
-          <img src="javascript.png" className="stackIcons2"></img>
-          <img src="iconfinder_react_1296845.png" className="stackIcons2"></img>
-          <img src="iconfinder_ruby_308447.png" className="stackIconsRails"></img>
-          <img src="iconfinder_ruby-on-rails_3069735.png" className="stackIcons"></img>
+          <img src={javascript} className="stackIcons2"></img>
+          <img src={react} className="stackIcons2"></img>
+          <img src={ruby} className="stackIconsRails"></img>
+          <img src={rails} className="stackIcons"></img>
             <br></br>
-          <img src="iconfinder_postgresql_4691328.png" className="stackIcons"></img>
-          <img src="iconfinder_css-3_4961937.png" className="stackIcons"></img>
-          <img src="iconfinder_50-html5_104453.png" className="stackIcons2"></img>
-          <img src="iconfinder_Github_1298743.png" className="stackIcons"></img>
+          <img src={postgresql} className="stackIcons"></img>
+          <img src={css3} className="stackIcons"></img>
+          <img src={html5} className="stackIcons2"></img>
+          <img src={github}className="stackIcons"></img>
        <div> 
         <strong>+</strong> 
         ActiveRecord, Sinatra,
